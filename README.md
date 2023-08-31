eb90a6269ed51a0600cc48f62e242be0b098aaa3
Brandon EtteR <brandonetter@gmail.com>
2023-08-31 15:52:18 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# NextComet v0.1.0
+
+## Server Sent Events in NextJS w/Mongo
+
+### Usage
+
+1. Create next api route:
+
+```js
+// app/api/SSE/route.ts
+import longPoll, { poller, PollLogger } from "@nextcomet";
+
+export async function GET() {
+  await MongooseConnect(); // connect to your DB
+
+  // Define polling settings
+  const pollSettings = {
+    model: ChatModel, //<-- Mongoose Model
+
+    //Event will run on DB insert
+    insert: (notify: PollLogger, change: any) => {
+      notify.log({
+        tag: "insert", // <-- tag messages however you'd like for the frontend
+        message: change.fullDocument.message,
+        id: change.fullDocument._id,
+      });
+    },
+    //Event will run on DB delete
+    delete: (notify: PollLogger, change: any) => {
+      notify.log({
+        tag: "delete",
+        message: "Chat deleted",
+        id: change.documentKey._id,
+      });
+    },
+  };
+
+  const responseStream = new TransformStream();
+  poller(responseStream, pollSettings);
+  return longPoll(responseStream);
+}
+```
+
+2. Use it in the frontend on a "use client" component:
+
+```js
+import { listenSSE } from "@nextcomet";
+interface ChatMessage {
+  message: string;
+  id: string;
+}
+
+// Begin listening for changes to the database
+// handleNewMessage is a callback that receives the changes
+// "/api/SSE" is the API resource we setup in step
+
+const [messages, setMessages] = useState<ChatMessage[]>([]);
+
+useEffect(() => {
+  listenSSE(handleNewMessage, "/api/SSE");
+}, []);
+
+// our message handling callback
+const handleNewMessage = (message: any) => {
+  switch (message.tag) {
+    case "insert": // <-- catch messages by tag defined in the API route
+      setMessages((messages) => [...messages, message]);
+      break;
+    case "delete":
+      console.log("Error: ", message);
+      break;
+  }
+};
+```
+
+Done! You're now getting live updates from your Database
---
+++ src/index.ts
@@ -0,0 +1,105 @@
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+interface Notify {
+  log: (message: { message: string; id: string; tag: string }) => void;
+  complete: (data: any) => void;
+  error: (error: Error | any) => void;
+  close: () => void;
+}
+export type PollLogger = Notify;
+export const listenSSE = (
+  callback: (event: MessageEvent<any>) => { cancel?: true } | void,
+  route: string
+) => {
+  const eventSource = new EventSource(route, {
+    withCredentials: true,
+  });
+  eventSource.onmessage = (event) => {
+    const result = callback(JSON.parse(event.data));
+    if (result?.cancel) {
+      eventSource.close();
+    }
+  };
+  return {
+    close: () => {
+      eventSource.close();
+    },
+  };
+};
+export interface PollSettings {
+  model: any;
+  insert?: (notify: PollLogger, change: any) => void;
+  delete?: (notify: PollLogger, change: any) => void;
+  update?: (notify: PollLogger, change: any) => void;
+}
+export async function poller(
+  responseStream: TransformStream,
+  pollSettings: PollSettings
+) {
+  const eventEmitter = pollSettings.model.watch();
+  const writer = responseStream.writable.getWriter();
+  const encoder = new TextEncoder();
+  let closed = false;
+  async function longPoller(notify: PollLogger) {
+    eventEmitter.on("change", (change: any) => {
+      if (change.operationType === "delete" && pollSettings.delete) {
+        pollSettings.delete(notify, change);
+      } else if (change.operationType === "insert" && pollSettings.insert) {
+        pollSettings.insert(notify, change);
+      } else if (change.operationType === "update" && pollSettings.update) {
+        pollSettings.update(notify, change);
+      }
+    });
+
+    while (true) {
+      await delay(1000);
+    }
+  }
+  longPoller({
+    log: (msg: { message: string }) =>
+      writer.write(encoder.encode("data: " + JSON.stringify(msg) + "\n\n")),
+    complete: (obj: any) => {
+      writer.write(encoder.encode("data: " + JSON.stringify(obj) + "\n\n"));
+      if (!closed) {
+        writer.close();
+        closed = true;
+      }
+    },
+    error: (err: Error | any) => {
+      writer.write(encoder.encode("data: " + err?.message + "\n\n"));
+      if (!closed) {
+        writer.close();
+        closed = true;
+      }
+    },
+    close: () => {
+      if (!closed) {
+        writer.close();
+        closed = true;
+      }
+    },
+  })
+    .then(() => {
+      if (!closed) {
+        writer.close();
+      }
+    })
+    .catch(() => {
+      if (!closed) {
+        writer.close();
+      }
+    });
+}
+
+export default function longPoll(responseStream: TransformStream) {
+  return new Response(responseStream.readable, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "text/event-stream; charset=utf-8",
+      Connection: "keep-alive",
+      "Cache-Control": "no-cache, no-transform",
+      "X-Accel-Buffering": "no",
+      "Content-Encoding": "none",
+    },
+  });
+}
